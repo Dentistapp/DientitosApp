53f982797fa68b133955367893df3597bd8dbc0c
Itzel Gomez <gomi1901@gmail.com>
2019-01-17 14:57:26 -0600

    boton SignOut  sirve

--- dientitosApp/dientitosApp/Controllers/PerfilViewController.swift
+++ dientitosApp/dientitosApp/Controllers/PerfilViewController.swift
@@ -7,14 +7,69 @@
 //
 
 import UIKit
+import Firebase
 
 class PerfilViewController: UIViewController {
 
+     let viewC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "loginVC")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupNavBar()
 
         // Do any additional setup after loading the view.
     }
+    
+    // MARK: - Custom Sign Out Button
+    
+    let signOutButton = UIBarButtonItem(title: "Sign Out", style: .done, target: self, action: #selector(handleSignOutButtonTapped))
+    
+    
+    @objc func handleSignOutButtonTapped(_ sender: Any){
+        
+        
+        if Auth.auth().currentUser != nil {
+            do {
+                try Auth.auth().signOut()
+                print("Salimooos")
+                self.present(viewC, animated: true, completion: nil)
+                
+            }
+            catch {
+            }
+        }
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    lazy var navBar: UINavigationBar = {
+        let navBar: UINavigationBar = UINavigationBar(frame: .zero)
+        let navItem = UINavigationItem(title: "Perfil")
+        navItem.rightBarButtonItem = signOutButton
+        navBar.setItems([navItem], animated: false)
+        navBar.translatesAutoresizingMaskIntoConstraints = false
+        return navBar
+    }()
+    
+    func setupNavBar() {
+        view.backgroundColor = .white
+        self.view.addSubview(navBar)
+        NSLayoutConstraint.activate([
+            navBar.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+            navBar.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            navBar.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            navBar.heightAnchor.constraint(equalToConstant: 44)
+            ])
+        
+        
+    }
+   
+
     
 
     /*
