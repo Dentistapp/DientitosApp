4f7cd3a1b7dbcf5450ce844e0bf586f7bde8ff98
Adrian Azpitarte <azpitarte14_@hotmail.com>
2019-01-30 21:49:21 -0600

    errorWithTest:(

--- dientitosApp/dientitosAppUITests/dientitosAppUITests.swift
+++ dientitosApp/dientitosAppUITests/dientitosAppUITests.swift
@@ -21,6 +21,8 @@ class dientitosAppUITests: XCTestCase {
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
+    
+
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
@@ -30,5 +32,26 @@ class dientitosAppUITests: XCTestCase {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
+    
+    func testAppLogginSucces() {
+        let app = XCUIApplication()
+        let emailTextField = app.textFields["emailTextField"]
+        emailTextField.tap()
+        emailTextField.typeText("a@a.com")
+        
+        let passwordTextField = app.secureTextFields["passwordTextField"]
+        passwordTextField.tap()
+        passwordTextField.typeText("123456")
+        
+        let loginbutton = app.buttons["singinbutton"]
+        loginbutton.tap()
+        XCTAssertTrue(app.isDisplayMusic)
+    }
 
+}
+
+extension XCUIApplication{
+    var isDisplayMusic: Bool{
+        return otherElements["HomeView"].exists
+    }
 }
